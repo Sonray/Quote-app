29c53c834f2990effefeccccc9d28a1392851e8f
David Okwacha <osweredavid@gmail.com>
2020-08-22 13:25:13 +0300

    Added constructor to the project

--- src/app/custom-qoute.ts
+++ src/app/custom-qoute.ts
@@ -1,2 +1,25 @@
 export class CustomQoute {
+
+    message: String;
+    quote: Quoter;
+    quoteArray: Array<Quoter>;
+    
+    loadQuote(){
+        this.quoteArray = [
+            new Quoter("I am a loner", "David", "Denis", "2020-10-13" )
+        ]
+        this.message = this.quoteArray[1].quoteName,this.quoteArray[1].quoteAurthor,this.quoteArray[1].quotedDay,this.quoteArray[1].name;
+    }
+    
+}
+
+export class Quoter {
+
+    constructor( public quoteName: String, public quoteAurthor: String, 
+        public name: String, public quotedDay:String ){
+            this.name = name,
+            this.quoteAurthor = quoteAurthor,
+            this.quoteName = quoteName,
+            this.quotedDay = quotedDay
+    }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomQoute } from '../custom-qoute'
 
 @Component({
   selector: 'app-quote',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit(): void {
