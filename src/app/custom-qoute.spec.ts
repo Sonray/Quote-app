7670c0736fdc06bc1c6d64a9734e86114b814bfb
David Okwacha <osweredavid@gmail.com>
2020-08-25 10:32:41 +0300

    Auto-generated commit

--- src/app/custom-qoute.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CustomQoute } from './custom-qoute';
-
-describe('CustomQoute', () => {
-  it('should create an instance', () => {
-    expect(new CustomQoute()).toBeTruthy();
-  });
-});
--- src/app/quote-constructor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { QuoteConstructor } from './quote-constructor';
-
-describe('QuoteConstructor', () => {
-  it('should create an instance', () => {
-    expect(new QuoteConstructor()).toBeTruthy();
-  });
-});
